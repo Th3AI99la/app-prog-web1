2680c66150304e6ee3e4aa964ff63dd65955ba81
Thalles Henrique Alves <104025889+Th3AI99la@users.noreply.github.com>
2025-02-17 21:13:50 -0300

    import-inicial

---
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_thalles_henrique/ProjetoThallesHenriqueApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projeto_thalles_henrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoThallesHenriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoThallesHenriqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/desenvolvimento/web/projeto_thalles_henrique/ProjetoThallesHenriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projeto_thalles_henrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoThallesHenriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
