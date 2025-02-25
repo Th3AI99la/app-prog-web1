c2313c73e92884783b6575b2f086ec5495f4596e
Th3AI99la <talles.th54@gmail.com>
2025-02-24 21:56:48 -0300

    update-Aula 2-HTML

---
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_thalles_henrique/controller/AlunoControler.java
@@ -0,0 +1,27 @@
+package br.ueg.desenvolvimento.web.projeto_thalles_henrique.controller;
+
+import java.util.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AlunoControler {
+    static List alunos = new ArrayList<>();
+    static {
+        alunos.add(Map.of("nome", "João", "email", "joao@localhost"));
+        alunos.add(Map.of("nome", "Maria", "email", "maria@localhost"));
+        alunos.add(Map.of("nome", "José", "email", "jose@localhost"));
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/alunos")
+    public String getHome(Model model) {
+        model.addAttribute("alunos", alunos);
+        return "alunos";
+    }
+}
