4065de0bbe030cec28e8375a8b05b3b94d414c95
Th3AI99la <talles.th54@gmail.com>
2025-03-17 20:29:47 -0300

    uodate

--- src/main/java/br/ueg/desenvolvimento/web/projeto_thalles_henrique/controller/AlunoController.java
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_thalles_henrique/controller/AlunoController.java
@@ -7,10 +7,9 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @Controller
 public class AlunoController { // Controller para gerenciar alunos
@@ -40,10 +39,7 @@ public class AlunoController { // Controller para gerenciar alunos
     }
 
     @PostMapping("/alunos/cadastrar")
-    public String postForm(
-            @RequestParam String nome,
-            @RequestParam String email) {
-
+    public String postForm(@RequestParam String nome, @RequestParam String email) {
         System.out.println("Nome: " + nome);
         System.out.println("Email: " + email);
 
@@ -53,5 +49,42 @@ public class AlunoController { // Controller para gerenciar alunos
         return "redirect:/alunos";
     }
 
-    
+    @GetMapping("/alunos/create")
+    public String getCreate() {
+        return "aluno-create";
+    }
+
+    @PostMapping("/alunos/create")
+    public String postCreate(@RequestParam String nome, @RequestParam String email) {
+        alunos.add(Map.of("nome", nome, "email", email));
+        return "redirect:/alunos";
+    }
+
+    @GetMapping("/alunos/update/{id}")
+    public String getUpdate(@PathVariable int id, Model model) {
+        model.addAttribute("aluno", alunos.get(id));
+        model.addAttribute("id", id);
+        return "aluno-update";
+    }
+
+    @PostMapping("/alunos/update")
+    public String postUpdate(@RequestParam int id, @RequestParam String nome, @RequestParam String email) {
+        alunos.set(id, Map.of("nome", nome, "email", email));
+        return "redirect:/alunos";
+    }
+
+    @GetMapping("/alunos/delete/{id}")
+    public String getDelete(@PathVariable int id, Model model) {
+        model.addAttribute("aluno", alunos.get(id));
+        model.addAttribute("id", id);
+
+        return "aluno-delete";
+    }
+
+    @PostMapping("/alunos/delete")
+    public String postDelete(@RequestParam int id) {
+        alunos.remove(id);
+        return "redirect:/alunos";
+    }
+
 }
